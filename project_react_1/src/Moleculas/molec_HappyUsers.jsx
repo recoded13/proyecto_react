f838c4056059e93be638223a7105b6283b285c3a
Viktor.A <viktor.antonyshyn@gmail.com>
2023-06-29 12:19:48 +0200

    Anado una carpeta Moleculas y una molecula HappyUsers

---
+++ project_react_1/src/Moleculas/molec_HappyUsers.jsx
@@ -0,0 +1,13 @@
+import icon_HappyUsers from '../Atomos/Icons/icon_HappyUsers'
+
+const HappyUsers = () => {
+    return (
+        <div class="people">
+             <img src={icon_HappyUsers} alt="Happy Users" />
+             <span>231</span>
+             <span class="texto">Happy Users</span>
+        </div>
+    )
+}
+
+export default HappyUsers
